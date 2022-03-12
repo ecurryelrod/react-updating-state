9384a1a882a46f7c943b038b7870119ebe794bfd
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-12 15:00:19 -0700

    LIghtSwitch component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick'
+// import LightSwitch from './components/LIghtSwitch'
 
 ReactDOM.render(<ClickityClick />, document.getElementById('root'));
