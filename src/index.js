42d3c0047d17ccc0f6332fbb0e1aa40920f6f2ba
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-12 15:01:05 -0700

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick'
+import LightSwitch from './components/LIghtSwitch';
 // import LightSwitch from './components/LIghtSwitch'
 
 ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+// ReactDOM.render(<LightSwitch />, document.getElementById('root'));
+
